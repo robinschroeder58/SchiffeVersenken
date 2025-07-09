4afc1dd5e7c67e0f17a344db957c19cecb2a9af1
robinschroeder58 <robinschroeder138@gmail.com>
2025-07-09 20:13:19 +0200

    added getMenuShipPlacement()

--- src/Main.java
+++ src/Main.java
@@ -7,3 +7,14 @@ public class Main {
 
     }
 }
+
+    public static String getMenuShipPlacement(int numberDestroyer, int numberCruiser, int numberBattleship) {
+
+        return
+                "1: Zerstörer platzieren: " + numberDestroyer + "/4" + "\n" +
+                        "2: Kreuzer platzieren: " + numberCruiser + "/2" + "\n" +
+                        "3: Schlachtschiff platzieren: " + numberBattleship + "/1" + "\n" +
+                        "4: Bestätige Schiffeplatzierung" + "\n" +
+                        "5: Karte ansehen" + "\n" +
+                        "6: Spiel beenden";
+    }
